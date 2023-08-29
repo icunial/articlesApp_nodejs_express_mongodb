1c098acf66992c92f218f04de890b9e38c2fe413
Ignacio Cunial <igsecu@hotmail.com>
2023-08-28 23:10:29 -0300

    Created check db connection and errors events -> index.js

--- index.js
+++ index.js
@@ -8,6 +8,16 @@ const db = mongoose.connection;
 
 const PORT = 5000 || process.env.PORT;
 
+// Check connection
+db.once("open", () => {
+  console.log("Connected to MongoDB");
+});
+
+// Check for DB errors
+db.on("error", (error) => {
+  console.log(error);
+});
+
 app.get("/", (req, res) => {
   res.send("ArticlesApp - NodeJS / Express / MongoDB");
 });
