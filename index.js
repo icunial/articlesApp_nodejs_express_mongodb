e5219a14b790d86512d5a5ce2f813a0966fa3018
Ignacio Cunial <igsecu@hotmail.com>
2023-08-29 23:47:33 -0300

    Added articles router

--- index.js
+++ index.js
@@ -5,9 +5,10 @@ const mongoose = require("mongoose");
 
 mongoose.connect("mongodb://localhost/articles_db");
 const db = mongoose.connection;
-const Article = require("./models/Article");
 
 const PORT = 5000 || process.env.PORT;
+
+const router = require("./routes/articles");
 
 // Check connection
 db.once("open", () => {
@@ -33,90 +34,8 @@ app.use(express.urlencoded({ extended: false }));
   })
 ); */
 
-app.get("/", async (req, res) => {
-  const articles = await Article.find({});
-  res.status(200).json({
-    statusCode: 200,
-    data: articles,
-  });
-});
-
-// Get Single Article
-app.get("/:id", async (req, res) => {
-  const { id } = req.params;
-  const articleFound = await Article.findById(id);
-  if (articleFound) {
-    return res.status(200).json({
-      statusCode: 200,
-      data: articleFound,
-    });
-  }
-});
-
-// Create an Article
-app.post("/", async (req, res) => {
-  const { title, author, body } = req.body;
-
-  // Validations
-  if (!title) {
-    return res.status(400).json({
-      statusCode: 400,
-      msg: `Title is required!`,
-    });
-  }
-  if (!author) {
-    return res.status(400).json({
-      statusCode: 400,
-      msg: `Author is required!`,
-    });
-  }
-  if (!body) {
-    return res.status(400).json({
-      statusCode: 400,
-      msg: `Body is required!`,
-    });
-  }
-
-  const articleCreated = await Article.create({ title, author, body });
-  if (articleCreated) {
-    return res.status(201).json({
-      statusCode: 201,
-      data: articleCreated,
-    });
-  }
-});
-
-// Update Article
-app.put("/:id", async (req, res) => {
-  const { id } = req.params;
-  const body = req.body;
-
-  const articleUpdated = await Article.updateOne({ _id: id }, { ...body });
-  if (articleUpdated) {
-    const articleFound = await Article.findById(id);
-    if (articleFound) {
-      return res.status(200).json({
-        statusCode: 200,
-        data: articleFound,
-      });
-    }
-  }
-});
-
-// Delete Article
-app.delete("/:id", async (req, res) => {
-  const { id } = req.params;
-  const articleToDelete = await Article.findById(id);
-  if (articleToDelete) {
-    const deleted = await Article.deleteOne({ _id: id });
-    if (deleted) {
-      return res.status(200).json({
-        statusCode: 200,
-        data: articleToDelete,
-      });
-    }
-  }
-});
+// Add router
+app.use("/", router);
 
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}...`);
