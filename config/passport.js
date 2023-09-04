6842236fab957b176c2e9e83568c8da686af059c
Ignacio Cunial <igsecu@hotmail.com>
2023-09-03 21:13:34 -0300

    Updated passport config file -> removed imported database.js file

--- config/passport.js
+++ config/passport.js
@@ -1,6 +1,5 @@
 const LocalStrategy = require("passport-local").Strategy;
 const User = require("../models/User");
-const config = require("./database");
 const bcrypt = require("bcryptjs");
 
 module.exports = (passport) => {
