c7ffb2769dc305435609094d3b868c534b4373f7
Ignacio Cunial <igsecu@hotmail.com>
2023-09-04 22:23:17 -0300

    Updated user register test -> Username is required

--- routes/users.test.js
+++ routes/users.test.js
@@ -45,4 +45,15 @@ describe("User Register", () => {
     expect(response.status).toBe(400);
     expect(response.body.msg).toBe("Email is required!");
   });
+  it("it should return a 400 status code -> Username is required", async () => {
+    const user = {
+      name: "User 1",
+      email: "user1@email.com",
+      password: "1234",
+      password2: "1234",
+    };
+    const response = await request(app).post("/users/register").send(user);
+    expect(response.status).toBe(400);
+    expect(response.body.msg).toBe("Username is required!");
+  });
 });
